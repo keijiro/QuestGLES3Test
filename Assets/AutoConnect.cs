1a78f3f242432f48148f1f79d73cfa972bf1d3df
Keijiro Takahashi <keijiro@gmail.com>
2024-06-14 16:45:18 +0900

    initial commit

---
+++ Assets/AutoConnect.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using System.Linq;
+using Klak.Ndi;
+
+public sealed class AutoConnect : MonoBehaviour
+{
+    void Update()
+    {
+        var sources = NdiFinder.sourceNames.ToArray();
+        if (sources.Length > 0)
+        {
+            GetComponent<NdiReceiver>().ndiName = sources[0];
+            enabled = false;
+        }
+    }
+}
